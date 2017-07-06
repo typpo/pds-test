cc0416acef0986a81a233b151508a6d096424fbf
Ian Webster <ianw_github@ianww.com>
2017-07-05 17:03:56 -0700

    read in all pds4 definition files given a directory

--- test.py
+++ test.py
@@ -1,5 +1,18 @@
+import os
+import fnmatch
+
 from pds4_tools import pds4_read
 
-struct_list = pds4_read('./data/dph_example_archive_VG2PLS/bundle.xml')
+#struct_list = pds4_read('./data/ldex_20161118/data_calibrated/collection_ldex_data_calibrated.xml')
+#struct_list = pds4_read('./data/ldex_20161118/bundle_ladee_ldex.xml')
 
-print struct_list
+path = './data/ldex_20161118/'
+for dirpath, dirname, files in os.walk(path):
+    for filepath in fnmatch.filter(files, '*.xml'):
+        fullpath = os.path.join(dirpath, filepath)
+        struct_list = pds4_read(fullpath)
+
+        for struct in struct_list:
+            print '*' * 80
+            print struct
+            print struct.info()
