878e9b93b4cce2bbeb439631ef5618660810cb2d
Ian Webster <ianw_github@ianww.com>
2017-07-05 17:05:10 -0700

    move things into a function

--- test.py
+++ test.py
@@ -3,16 +3,18 @@ import fnmatch
 
 from pds4_tools import pds4_read
 
-#struct_list = pds4_read('./data/ldex_20161118/data_calibrated/collection_ldex_data_calibrated.xml')
-#struct_list = pds4_read('./data/ldex_20161118/bundle_ladee_ldex.xml')
+def load_hierarchy(path):
+    for dirpath, dirname, files in os.walk(path):
+        for filepath in fnmatch.filter(files, '*.xml'):
+            fullpath = os.path.join(dirpath, filepath)
+            struct_list = pds4_read(fullpath)
 
-path = './data/ldex_20161118/'
-for dirpath, dirname, files in os.walk(path):
-    for filepath in fnmatch.filter(files, '*.xml'):
-        fullpath = os.path.join(dirpath, filepath)
-        struct_list = pds4_read(fullpath)
-
-        for struct in struct_list:
             print '*' * 80
-            print struct
-            print struct.info()
+            print fullpath
+            for struct in struct_list:
+                print '-' * 80
+                print struct
+                print struct.info()
+
+if __name__ == '__main__':
+    load_hierarchy('./data/ldex_20161118/')
