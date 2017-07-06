6ce10595fbbbf2c887effd4550fc55fafc3f441d
Ian Webster <ianw_github@ianww.com>
2017-07-06 07:53:05 -0700

    write all files as fits in parallel directory structure

--- test.py
+++ test.py
@@ -24,10 +24,13 @@ def load_hierarchy(path):
         # http://sbndev.astro.umd.edu/wiki/Python_PDS4_Tools#pds4_read
         for struct in struct_list:
             print '-' * 80
-            if type(struct) == TableStructure:
-                # xpath tester: https://codebeautify.org/Xpath-Tester#
-                fields = [elt.text for elt in struct.label.findall('.//Field_Character/name')]
-                formats = [elt.text for elt in struct.label.findall('.//Field_Character/field_format')]
+            if type(struct) != TableStructure:
+                print 'Unknown struct type encountered:', type(struct)
+                continue
+
+            # xpath tester: https://codebeautify.org/Xpath-Tester#
+            fields = [elt.text for elt in struct.label.findall('.//Field_Character/name')]
+            formats = [elt.text for elt in struct.label.findall('.//Field_Character/field_format')]
 
             # See astropy docs for writing fits tables:
             # http://docs.astropy.org/en/stable/io/fits/#creating-a-new-table-file
@@ -41,15 +44,24 @@ def load_hierarchy(path):
             if len(cols) < 1:
                 continue
 
+            print 'Writing fits file...'
             coldef = fits.ColDefs(cols)
             tbhdu = fits.BinTableHDU.from_columns(coldef)
 
             prihdr = fits.Header()
-            prihdr['COMMENT'] = 'Converted by PDSKit'
+            prihdr['COMMENT'] = 'Converted by PDSKit from %s' % xml_path
             prihdu = fits.PrimaryHDU(header=prihdr)
 
             thdulist = fits.HDUList([prihdu, tbhdu])
-            thdulist.writeto('table.fits')
+
+            out_dirpath = './out/%s' % os.path.dirname(xml_path)
+            if not os.path.exists(out_dirpath):
+                os.makedirs(out_dirpath)
+
+            out_filename = os.path.basename(xml_path).split('.')[0]
+            out_path = '%s.fits' % os.path.join(out_dirpath, out_filename)
+            if not os.path.exists(out_path):
+                thdulist.writeto(out_path)
 
             num_records += len(struct.data)
     print 'Total number of records:', num_records
